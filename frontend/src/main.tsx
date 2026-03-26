3fada2aff3af2c171d4a93e0ad73d4a0778086a2
tusimbe <tusimbe@gmail.com>
2026-03-26 11:23:08 +0800

    feat(frontend): scaffold React + TypeScript + Vite + Ant Design project

---
+++ frontend/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App: React.FC = () => <div>数据中台管理后台</div>;
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
